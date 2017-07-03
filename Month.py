8bd5ba73e5fdb4633d6d06338ba560ab1a97054e
akuzzm <akuzzm@gmail.com>
2017-07-03 16:08:07 +0300

    Create Month.py

---
+++ Month.py
@@ -0,0 +1,12 @@
+def season(month):
+  print "Month: ", month
+  if month in [12, 1, 2]:
+    return "winter"
+  if month in ([3, 4, 5]:
+    return "spring"
+  if month in [6, 7, 8]:
+    return "summer"
+  if month in [9, 10, 11]:
+    return "autumn"
+  else:
+    return "Write corect month!"
