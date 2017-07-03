eb876d9252d59ca6d512db57b40f1fab664f1851
akuzzm <akuzzm@gmail.com>
2017-07-03 16:18:30 +0300

    Create Names.py

---
+++ Names.py
@@ -0,0 +1,16 @@
+names = ['Ann', 'Fillip', 'Monika', 'Ross', 'Sam', 'Alf', 'Roy', 'Sasha', 'Leon']
+
+for el in names:
+  print el
+  
+for el in range(len(names)):
+  print el
+
+for el in names:
+  print el[0]
+  
+for el in names:
+  print el[-1]
+
+for el in names:
+  print el, len(el)
