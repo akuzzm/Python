41990aac84deb8482ff0ef9dac972424a527ab2b
akuzzm <akuzzm@gmail.com>
2017-08-18 20:21:41 +0300

    Cats and dogs were added

--- Pet.py
+++ Pet.py
@@ -4,7 +4,8 @@ class Pet(object):
       self.species = species
       
    def get_name(self):
-      return self.name
+      text = "Hello, my name is " + self.name 
+      return text
       
    def get_species(self):
       return self.species
@@ -12,3 +13,24 @@ class Pet(object):
    def __str__(self):
       return "{} is a {}".format (self.name, self.species)
 
+class Dog(Pet):
+    def __init__(self, name, chases_cats):
+        Pet.__init__(self, name, "Dog")
+        self.chases_cats = chases_cats
+
+    def is_chases_cats(self):
+        return self.chases_cats
+
+    def say_smth(self):
+        print "Woof, woof!!!"
+
+class Cat(Pet):
+    def __init__(self, name, hates_dogs):
+        Pet.__init__(self, name, "Cat")
+        self.hates_dogs = hates_dogs
+
+    def is_hates_dogs(self):
+        return self.hates_dogs
+
+    def say_smth(self):
+        print "Meow, meow!!!"
