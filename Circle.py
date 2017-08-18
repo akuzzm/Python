09341424cf674891aa94711ce8ed8c7547bf8ac5
akuzzm <akuzzm@gmail.com>
2017-08-18 19:33:35 +0300

    It`s better

--- Circle.py
+++ Circle.py
@@ -6,20 +6,27 @@ class Circle:
       
     def area(self):
        if self.x > 0 and self.y > 0:
-            return "area =", self.r * self.r * 3.14
+         area = self.r * self.r * 3.14
+         return area
        else:
             return "Enter correct data!"
 
     def length(self):
         if self.x > 0 and self.y > 0:
-            return "length =", 2 * self.r * 3.14
+            length = 2 * self.r * 3.14
+            return length
         else:
             return "Enter correct data!"
 
     def __str__(self):
         if self.x > 0 and self.y > 0:
             return "x = {}, y = {}, r = {}, area = {}, length = {}" \
-               .format(self.x, self.y, self.r, self.r * self.r * 3.14,
-                       2 * self.r * 3.14)
+               .format(self.x, self.y, self.r, self.area(),
+                       self.length())
         else:
             return "Enter correct data!"
+        
+if __name__=="__main__":
+    
+    circle_a=Circle(x=2, y=3, r=5)
+    print circle_a
